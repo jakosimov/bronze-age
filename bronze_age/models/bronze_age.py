Add recurrence, and fix entropy calculation and MLP and Linear behaviour with explanations

bronze_age/models/bronze_age.py
@@ -32,16 +32,20 @@ def differentiable_argmax(x):
     # We add the softmax and subtract the detached softmax, letting the gradient pass through
     return y_hard.detach() + y_soft - y_soft.detach()
 
+
 class GumbelSoftmax(nn.Module):
     def __init__(self, config: Config):
         super().__init__()
         self.tau = config.temperature
-    
+
     def forward(self, x):
         if self.training:
             return F.gumbel_softmax(x, hard=True, tau=self.tau)
         else:
-            return F.one_hot(x.argmax(dim=-1), x.shape[-1]).to(dtype=x.dtype, device=x.device) # exact ones are needed for Decision Trees for some reason
+            return F.one_hot(x.argmax(dim=-1), x.shape[-1]).to(
+                dtype=x.dtype, device=x.device
+            )  # exact ones are needed for Decision Trees for some reason
+
 
 class DifferentiableArgmax(nn.Module):
     def __init__(self, config: Config):
@@ -51,7 +55,9 @@ class DifferentiableArgmax(nn.Module):
         if self.training:
             return differentiable_argmax(x)
         else:
-            return F.one_hot(x.argmax(dim=-1), x.shape[-1]).to(dtype=x.dtype, device=x.device) # exact ones are needed for Decision Trees for some reason
+            return F.one_hot(x.argmax(dim=-1), x.shape[-1]).to(
+                dtype=x.dtype, device=x.device
+            )  # exact ones are needed for Decision Trees for some reason
 
 
 class MLP(torch.nn.Module):
@@ -75,19 +81,40 @@ class MLP(torch.nn.Module):
         for bn in self.bns:
             bn.reset_parameters()
 
-    def forward(self, x):
+    def forward(self, x, return_explanation=False):
         for i, lin in enumerate(self.lins[:-1]):
             x = lin(x)
             x = self.bns[i](x)
             x = F.relu(x)
             x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.lins[-1](x)
+        if return_explanation:
+            return x, None
         return x
-    
+
+
+class Linear(torch.nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Linear, self).__init__()
+        self.lin = torch.nn.Linear(in_channels, out_channels)
+
+    def forward(self, x, return_explanation=False):
+        x = self.lin(x)
+        if return_explanation:
+            return x, None
+        return x
 
 
 class BronzeAgeLayer(nn.Module):
-    def __init__(self, in_channels, out_channels, config: Config, layer_type: LayerType = None, non_linearity=None, name=None):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        config: Config,
+        layer_type: LayerType | None = None,
+        non_linearity=None,
+        name=None,
+    ):
         super().__init__()
         self.config = config
         self.name = name
@@ -96,14 +123,18 @@ class BronzeAgeLayer(nn.Module):
 
         if name is not None:
             self.__name__ = name
-        
+
         layer_type = layer_type or config.layer_type
         if layer_type == LayerType.LINEAR:
-            self.f = torch.nn.Linear(in_channels, out_channels)
+            self.f = Linear(in_channels, out_channels)
         elif layer_type == LayerType.MLP:
-            self.f = MLP(in_channels, config.hidden_units, out_channels, 2, config.dropout)
+            self.f = MLP(
+                in_channels, config.hidden_units, out_channels, 2, config.dropout
+            )
         elif layer_type == LayerType.DEEP_CONCEPT_REASONER:
-            self.f = ConceptReasonerModule(in_channels, out_channels, config.concept_embedding_size, config)
+            self.f = ConceptReasonerModule(
+                in_channels, out_channels, config.concept_embedding_size, config
+            )
         elif layer_type == LayerType.GLOBAL_DEEP_CONCEPT_REASONER:
             self.f = GlobalConceptReasonerModule(in_channels, out_channels, config)
         else:
@@ -117,7 +148,7 @@ class BronzeAgeLayer(nn.Module):
             self.non_linearity = DifferentiableArgmax(config)
         else:
             raise NotImplementedError
-        
+
         if config.evaluation_nonlinearity is None:
             self.eval_non_linearity = self.non_linearity
         elif config.evaluation_nonlinearity == NonLinearity.GUMBEL_SOFTMAX:
@@ -126,7 +157,7 @@ class BronzeAgeLayer(nn.Module):
             self.eval_non_linearity = DifferentiableArgmax(config)
         else:
             raise NotImplementedError
-        
+
         self.non_linearity = non_linearity or self.non_linearity
         self.eval_non_linearity = non_linearity or self.eval_non_linearity
 
@@ -135,14 +166,24 @@ class BronzeAgeLayer(nn.Module):
             x1, explanation = self.f(x, return_explanation=True)
         else:
             x1, explanation = self.f(x), None
-            
+
         x2 = self.non_linearity(x1) if self.training else self.eval_non_linearity(x1)
 
-        entropy_loss = nn.functional.mse_loss(x2, x1, reduction="mean")
+        entropy_baseline = self.eval_non_linearity(x1)
+
+        entropy_loss = nn.functional.mse_loss(entropy_baseline, x1, reduction="mean")
         return x2, entropy_loss, explanation
-    
+
+
 class BronzeAgeDecisionTree(nn.Module):
-    def __init__(self, tree, out_channels: int, state_size: int, config: Config, use_linear_feature_combinations=False):
+    def __init__(
+        self,
+        tree,
+        out_channels: int,
+        state_size: int,
+        config: Config,
+        use_linear_feature_combinations=False,
+    ):
         super().__init__()
         self.tree = tree
         self.config = config
@@ -157,24 +198,50 @@ class BronzeAgeDecisionTree(nn.Module):
             neighbors = x[..., num_states:]
             neighbors_difference = neighbors[..., :, None] > neighbors[..., None, :]
             # remove diagonal and flatten rest
-            neighbors_difference = neighbors_difference[:, ~np.eye(neighbors_difference.shape[-1], dtype=bool)].reshape(
-                neighbors_difference.shape[0], -1
+            neighbors_difference = neighbors_difference[
+                :, ~np.eye(neighbors_difference.shape[-1], dtype=bool)
+            ].reshape(neighbors_difference.shape[0], -1)
+            x = np.concatenate(
+                (current_state, neighbors, neighbors_difference), axis=-1
             )
-            x = np.concatenate((current_state, neighbors, neighbors_difference), axis=-1)
         return x
-    
+
     @staticmethod
-    def from_data(x, y, out_channels: int, state_size: int, config: Config, use_linear_feature_combinations=False, layer_name=None):
-        tree = DecisionTreeClassifier(random_state=0, max_leaf_nodes=config.max_leaf_nodes)
-        x = BronzeAgeDecisionTree._preprocess_features(x, state_size, use_linear_feature_combinations)
+    def from_data(
+        x,
+        y,
+        out_channels: int,
+        state_size: int,
+        config: Config,
+        use_linear_feature_combinations=False,
+        layer_name=None,
+    ):
+        tree = DecisionTreeClassifier(
+            random_state=0, max_leaf_nodes=config.max_leaf_nodes
+        )
+        x = BronzeAgeDecisionTree._preprocess_features(
+            x, state_size, use_linear_feature_combinations
+        )
         tree.fit(x, y)
-        return BronzeAgeDecisionTree(tree, out_channels, state_size, config, use_linear_feature_combinations=use_linear_feature_combinations)
-    
+        return BronzeAgeDecisionTree(
+            tree,
+            out_channels,
+            state_size,
+            config,
+            use_linear_feature_combinations=use_linear_feature_combinations,
+        )
+
     def forward(self, x, return_explanation=False):
         x1 = x.cpu().detach().numpy()
-        x1 = self._preprocess_features(x1, self.state_size, self.use_linear_feature_combinations)
-        y  = torch.tensor(self.tree.predict(x1)).to(device=x.device, dtype=torch.long)
-        return F.one_hot(y, self.out_channels).to(dtype=x.dtype, device=x.device), torch.tensor(0.0).to(device=x.device), None
+        x1 = self._preprocess_features(
+            x1, self.state_size, self.use_linear_feature_combinations
+        )
+        y = torch.tensor(self.tree.predict(x1)).to(device=x.device, dtype=torch.long)
+        return (
+            F.one_hot(y, self.out_channels).to(dtype=x.dtype, device=x.device),
+            torch.tensor(0.0).to(device=x.device),
+            None,
+        )
 
 
 class BronzeAgeGNNLayer(MessagePassing):
@@ -187,15 +254,20 @@ class BronzeAgeGNNLayer(MessagePassing):
         bounding_parameter = config.bounding_parameter
         self.bounding_parameter = bounding_parameter
         if config.aggregation_mode == AggregationMode.STONE_AGE:
-            self.layer = BronzeAgeLayer(2*in_channels, out_channels, config)
-        elif config.aggregation_mode in [AggregationMode.BRONZE_AGE, AggregationMode.BRONZE_AGE_ROUNDED]:
+            self.layer = BronzeAgeLayer(2 * in_channels, out_channels, config)
+        elif config.aggregation_mode in [
+            AggregationMode.BRONZE_AGE,
+            AggregationMode.BRONZE_AGE_ROUNDED,
+        ]:
             bounding_parameter = config.bounding_parameter
             self.register_buffer("_Y_range", torch.arange(bounding_parameter).float())
-        
-            self.layer = BronzeAgeLayer(in_channels * (bounding_parameter + 1), out_channels, config)
+
+            self.layer = BronzeAgeLayer(
+                in_channels * (bounding_parameter + 1), out_channels, config
+            )
         else:
             raise NotImplementedError
-        
+
         if name is not None:
             self.__name__ = name
 
@@ -210,7 +282,10 @@ class BronzeAgeGNNLayer(MessagePassing):
         clamped_sum = torch.clamp(message_sums, min=0, max=self.bounding_parameter)
         if self.config.aggregation_mode == AggregationMode.STONE_AGE:
             return clamped_sum
-        elif self.config.aggregation_mode in [AggregationMode.BRONZE_AGE, AggregationMode.BRONZE_AGE_ROUNDED]:
+        elif self.config.aggregation_mode in [
+            AggregationMode.BRONZE_AGE,
+            AggregationMode.BRONZE_AGE_ROUNDED,
+        ]:
             states = F.elu(clamped_sum[..., None] - self._Y_range) - 0.5
             states = F.sigmoid(self.a * states)
             states = states.view(*states.shape[:-2], -1)
@@ -223,43 +298,82 @@ class BronzeAgeGNNLayer(MessagePassing):
     def update(self, inputs, x, return_explanation=False):
         combined = torch.cat((x, inputs), 1)
         return self.layer(combined, return_explanation=return_explanation)
-     
-    
+
+
 class BronzeAgeGNN(torch.nn.Module):
 
     def __init__(self, in_channels, out_channels, config: Config):
         super(BronzeAgeGNN, self).__init__()
         self.config = config
-        
+
         self.use_pooling = config.dataset.uses_pooling
         self.skip_connection = config.skip_connection
         self.config = config
-        
+
         state_size = config.state_size
         num_layers = config.num_layers
 
-        self.input = BronzeAgeLayer(in_channels, state_size, config, layer_type=LayerType.LINEAR if config.layer_type == LayerType.MLP else config.layer_type, name="InputLayer")
+        self.input = BronzeAgeLayer(
+            in_channels,
+            state_size,
+            config,
+            layer_type=(
+                LayerType.LINEAR
+                if config.layer_type == LayerType.MLP
+                else config.layer_type
+            ),
+            name="InputLayer",
+        )
 
-        final_layer_inputs = (num_layers + 1) * state_size if self.skip_connection else state_size
-        final_non_linearity = nn.LogSoftmax(dim=-1) if config.layer_type in [LayerType.LINEAR, LayerType.MLP] else None
-        if config.layer_type in [LayerType.LINEAR, LayerType.MLP] and config.loss_mode == LossMode.BINARY_CROSS_ENTROPY:
+        final_layer_inputs = (
+            (num_layers + 1) * state_size if self.skip_connection else state_size
+        )
+        final_non_linearity = (
+            nn.LogSoftmax(dim=-1)
+            if config.layer_type in [LayerType.LINEAR, LayerType.MLP]
+            else None
+        )
+        if (
+            config.layer_type in [LayerType.LINEAR, LayerType.MLP]
+            and config.loss_mode == LossMode.BINARY_CROSS_ENTROPY
+        ):
             final_non_linearity = nn.Sigmoid()
-        self.output = BronzeAgeLayer(final_layer_inputs, out_channels, config, non_linearity=final_non_linearity, name="PoolingLayer",)
+        self.output = BronzeAgeLayer(
+            final_layer_inputs,
+            out_channels,
+            config,
+            non_linearity=final_non_linearity,
+            name="PoolingLayer",
+        )
 
         self.stone_age = nn.ModuleList()
         for i in range(num_layers):
-            self.stone_age.append(BronzeAgeGNNLayer(state_size, state_size, config, name=f"StoneAgeLayer-{i}"))
-    
+            self.stone_age.append(
+                BronzeAgeGNNLayer(
+                    state_size, state_size, config, name=f"StoneAgeLayer-{i}"
+                )
+            )
+
     def forward(self, x, edge_index, batch=None, return_explanation=False):
-        x, loss_term, explanation = self.input(x.float(), return_explanation=return_explanation)
+        x, loss_term, explanation = self.input(
+            x.float(), return_explanation=return_explanation
+        )
         entropy = {"input": loss_term}
         explanations = {"input": explanation}
         xs = [x]
-        for layer in self.stone_age:
-            x, loss_term, explanation = layer(x, edge_index, return_explanation=return_explanation)
-            entropy[layer.__name__] = loss_term
-            explanations[layer.__name__] = explanation
-            xs.append(x)
+
+        # This should maybe be in the contructor for config
+        assert not (self.config.num_recurrent_iterations > 1 and self.skip_connection)
+
+        for iteration in range(self.config.num_recurrent_iterations):
+            for layer in self.stone_age:
+                x, loss_term, explanation = layer(
+                    x, edge_index, return_explanation=return_explanation
+                )
+                entropy[f"{layer.__name__}_{iteration}"] = loss_term
+                if iteration == 0:
+                    explanations[layer.__name__] = explanation
+                xs.append(x)
 
         if self.use_pooling:
             x = global_add_pool(x, batch)
@@ -272,14 +386,15 @@ class BronzeAgeGNN(torch.nn.Module):
 
         if not return_explanation:
             explanations = None
-    
+
         return x, entropy, explanations
-    
+
     def to_decision_tree(self, train_loader):
         decision_tree = deepcopy(self)
         inputs_train = defaultdict(list)
         outputs_train = defaultdict(list)
         current_mask = None
+
         def _hook(module, input, output, key=None):
             if key is None:
                 raise ValueError("Key must be provided")
@@ -291,12 +406,12 @@ class BronzeAgeGNN(torch.nn.Module):
             y = torch.argmax(y, dim=-1, keepdim=False)
             inputs_train[key].append(x.detach().cpu().numpy())
             outputs_train[key].append(y.detach().cpu().numpy())
-            
+
         hooks = []
         for name, module in decision_tree.named_modules():
             if isinstance(module, BronzeAgeLayer):
                 hooks.append(module.register_forward_hook(partial(_hook, key=name)))
-        
+
         decision_tree.eval()
         for data in train_loader:
             if hasattr(data, "train_mask"):
@@ -309,13 +424,27 @@ class BronzeAgeGNN(torch.nn.Module):
         for key in inputs_train.keys():
             inputs_train[key] = np.concatenate(inputs_train[key])
             outputs_train[key] = np.concatenate(outputs_train[key])
-        
+
         for key in inputs_train.keys():
             out_channels = decision_tree.get_submodule(key).out_channels
-            use_linear_feature_combinations = not (key == "input" or (key == "output" and not self.config.dataset.uses_pooling))
-            num_states = 0 if key == "output" and self.config.dataset.uses_pooling else self.config.state_size
-            decision_tree_module = BronzeAgeDecisionTree.from_data(inputs_train[key], outputs_train[key], out_channels, num_states, self.config, use_linear_feature_combinations=use_linear_feature_combinations, layer_name=key)
+            use_linear_feature_combinations = not (
+                key == "input"
+                or (key == "output" and not self.config.dataset.uses_pooling)
+            )
+            num_states = (
+                0
+                if key == "output" and self.config.dataset.uses_pooling
+                else self.config.state_size
+            )
+            decision_tree_module = BronzeAgeDecisionTree.from_data(
+                inputs_train[key],
+                outputs_train[key],
+                out_channels,
+                num_states,
+                self.config,
+                use_linear_feature_combinations=use_linear_feature_combinations,
+                layer_name=key,
+            )
             decision_tree.set_submodule(key, decision_tree_module)
-        
 
         return decision_tree

bronze_age/models/concept_reasoner.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 import torch
 
-from bronze_age.config import Config
+from bronze_age.config import BronzeConfig, Config
 
 
 class Logic:
@@ -256,7 +256,6 @@ class ConceptReasoningLayer(torch.nn.Module):
                     )
 
         return explanations
-    
 
 
 class GlobalConceptReasoningLayer(torch.nn.Module):
@@ -303,9 +302,7 @@ class GlobalConceptReasoningLayer(torch.nn.Module):
         else:
             return preds
 
-    def explain(
-        self, c, mode, concept_names=None, class_names=None, filter_attn=None
-    ):
+    def explain(self, c, mode, concept_names=None, class_names=None, filter_attn=None):
         assert mode in ["local", "global", "exact"]
 
         if concept_names is None:
@@ -339,12 +336,8 @@ class GlobalConceptReasoningLayer(torch.nn.Module):
                     minterm = []
                     for concept_idx in range(len(concept_names)):
                         c_pred = c[sample_idx, concept_idx]
-                        sign_attn = sign_attn_mask[
-                            0, concept_idx, target_class
-                        ]
-                        filter_attn = filter_attn_mask[
-                            0, concept_idx, target_class
-                        ]
+                        sign_attn = sign_attn_mask[0, concept_idx, target_class]
+                        filter_attn = filter_attn_mask[0, concept_idx, target_class]
 
                         # we first check if the concept was relevant
                         # a concept is relevant <-> the filter attention score is lower than the concept probability
@@ -411,7 +404,7 @@ def generate_names(n_concepts, in_channels, bounding_parameter):
 
 
 class ConceptReasonerModule(torch.nn.Module):
-    def __init__(self, n_concepts, n_classes, emb_size, config: Config):
+    def __init__(self, n_concepts, n_classes, emb_size, config: Config | BronzeConfig):
         super(ConceptReasonerModule, self).__init__()
         self.emb_size = emb_size
         self.n_classes = n_classes
@@ -448,7 +441,7 @@ class ConceptReasonerModule(torch.nn.Module):
 
 
 class GlobalConceptReasonerModule(torch.nn.Module):
-    def __init__(self, n_concepts, n_classes, config: Config):
+    def __init__(self, n_concepts, n_classes, config: Config | BronzeConfig):
         super(GlobalConceptReasonerModule, self).__init__()
         self.n_classes = n_classes
         self.concept_reasoner = GlobalConceptReasoningLayer(